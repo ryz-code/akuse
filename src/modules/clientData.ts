ee3c114cbd5e0d65bf0ac406a8f2b0b0265c5e80
ryzXD. <111454488+ryz-code@users.noreply.github.com>
2024-11-02 13:09:57 +0700

    Update clientData.ts

--- src/modules/clientData.ts
+++ src/modules/clientData.ts
@@ -1,1 +1,7 @@
-p
+import { ClientData } from "../types/types";
+
+export const clientData: ClientData = {
+  clientId: 12345,
+  redirectUri: 'akuse://index',
+  clientSecret: 'iA04TKLO3k3LaVWhxucJwck0glR6uhiv',
+};
